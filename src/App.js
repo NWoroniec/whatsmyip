c149a553cc781bb2feaa6c40dd2ca25c5858bf81
Saritha Sujith <pillai.sujith.saritha@gmail.com>
2022-03-05 13:00:58 +0100

    fetched api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ip, setIp] = useState('');
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const response = await fetch(
+					`https://geo.ipify.org/api/v2/country?apiKey=${process.env.REACT_APP_API_KEY}`
+				);
+				if (response.ok) {
+					const jsonResponse = await response.json();
+					console.log('response', jsonResponse);
+					return setIp(jsonResponse.ip);
+				} else {
+					console.error('Request failed!');
+				}
+			} catch (error) {
+				console.log(error.message);
+			}
+		};
+		getData();
+	}, []);
+
+	return (
+		<div>
+			<h2>IP ADDRESS</h2>
+			<p>Your ip:{ip}</p>
+		</div>
+	);
 }
 
 export default App;
